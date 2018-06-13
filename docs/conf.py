09ad8bb9a57db3b8e4f2424980f403993f0df20d
viik36 <vedantadas99@gmail.com>
2018-06-13 13:03:46 +0530

    index

--- docs/conf.py
+++ docs/conf.py
@@ -39,11 +39,10 @@ release = ''
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.imgmath',
     'sphinx.ext.autodoc',
     'sphinx.ext.intersphinx',
     'sphinx.ext.todo',
-    
+    'sphinx.ext.mathjax',
     'sphinx.ext.ifconfig',
 ]
 
