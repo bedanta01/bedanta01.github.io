699601c7237e3909b8a321bbc25be53e85988981
viik36 <vedantadas99@gmail.com>
2018-06-13 13:07:14 +0530

    index

--- docs/conf.py
+++ docs/conf.py
@@ -91,6 +91,7 @@ pygments_style = 'sphinx'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+mathjax_path = ['MathJax/MathJax.js']
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
